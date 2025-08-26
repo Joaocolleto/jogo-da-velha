8943f0d987ea64ee6468cee49471daf5c30af691
joaocolleto <colletodesouza@gmail.com>
2025-08-25 21:20:28 -0300

    NEW:Criação do tabuleiro 3x3

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ export default function Square(){
     <button className="square">1</button>
     <button className="square">2</button>
     <button className="square">3</button>
+
+    <div>
     <button className="square">4</button>
     <button className="square">5</button>
     <button className="square">6</button>
+    </div>
+
+    <div></div>
     <button className="square">7</button>
     <button className="square">8</button>
     <button className="square">9</button>
     </div>
+
+    
   )
 }
